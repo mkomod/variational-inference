070b9ae6077378033b8218f70a98dd9cb2d8bdd7
Desi <desi.r.ivanova@gmail.com>
2020-10-30 13:17:15 +0000

    Adding YML for conda env; fixed bias error

--- py/bayesreg_pyro.py
+++ py/bayesreg_pyro.py
@@ -16,17 +16,24 @@ class BayesianRegression(PyroModule):
         in_features, 
         out_features=1,
         bias=True,
+        bias_prior=None,
         weight_prior=None,
         sigma_prior=None,
     ):
         
         super().__init__()
-        self._weight_prior = weight_prior or pyro_dist.Normal(0., 1.).expand([out_features, in_features])# .to_event(2)
-        self._sigma_prior = sigma_prior or pyro_dist.HalfCauchy(scale=torch.tensor([1.0]))
+        self._bias_flag = bias
+        # setup the linear model
         self.linear = PyroModule[torch.nn.Linear](in_features, out_features, bias=bias)
+        # setup priors
+        self._weight_prior = weight_prior or pyro_dist.Normal(0., 1.).expand([out_features, in_features])#.to_event(2)
         self.linear.weight = PyroSample(self._weight_prior)
-        
-        
+        if self._bias_flag:
+            self._bias_prior = bias_prior or pyro_dist.Normal(0., 2.).expand([out_features])#.to_event(1)
+            self.linear.bias =  PyroSample(self._bias_prior)
+        self._sigma_prior = sigma_prior or pyro_dist.HalfCauchy(scale=torch.tensor([1.0]))
+            
+    
     def forward(self, x, y=None):
         sigma = pyro.sample("sigma",  self._sigma_prior)
         mean = self.linear(x).squeeze(-1)
@@ -35,6 +42,21 @@ class BayesianRegression(PyroModule):
         return mean
     
     
+    def generate_synthetic_data(x_data, num_synthetic_samples=100):
+        """returns: generated parameters and synthethic samples"""
+        w0 = self._weight_prior.sample()
+        sigma0 = self._sigma_prior.sample()
+        if self._bias_flag:
+            b0 = self._bias_prior.sample()
+            params = (b0, w0, sigma0)
+            mean = b0 + torch.mm(x_data, w0.T)
+        else:
+            params = (w0, sigma0)
+            mean = torch.mm(x_data, w0.T)
+        
+        return params, dist.Normal(mean, sigma).sample(torch.Size(num_synthetic_samples))
+        
+
 def run_vi(x_data, y_data, vi_engine, model, guide, num_iterations=int(1e4), num_post_samples=int(1e3)): 
     """
     Runs VI for a given number of iterations -- num_iterations
@@ -58,7 +80,7 @@ def run_vi(x_data, y_data, vi_engine, model, guide, num_iterations=int(1e4), num
         return elbos
     else:
         predictive = Predictive(
-            model, guide=guide, num_samples=num_post_samples, return_sites=("linear.weight", "sigma",)
+            model, guide=guide, num_samples=num_post_samples, return_sites=("linear.weight", "linear.bias", "sigma",)
         )
         samples = predictive(x_data)
         return elbos, samples
