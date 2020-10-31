7fda6b3d9b5d0a9cbd6f3bb88c7abe4e680cf5ba
mkomod <mkomodromos01@gmail.com>
2020-10-31 11:13:51 +0000

    fix: l and nu calculation

--- R/gpvi/gpvi.r
+++ R/gpvi/gpvi.r
@@ -1,5 +1,5 @@
 MC.SAMPLES <- 1e3                      # Monte Carlo Samples
-N.ITERATIONS <- 50                     # Number of iterations
+N.ITERATIONS <- 5                     # Number of iterations
 
 #' Computes the Gaussian Kernel K for vectors x1, x2
 kern.gauss <- function(x1, x2, l=1, s=1) {
@@ -17,13 +17,14 @@ V <- function(x, Y, theta) {
 # GPVI -----------------------------------------------------------------------
 
 # Generate synthetic data
-# Y <-
-# X <- 
+X <- 1:10
+Y <- 10 + 2 * sin(X) + 0.05 * X + rnorm(10, sd=2)
 n <- length(Y)
+theta <- 2
 
 K <- kern.gauss(X, X)
-l <- runif(n)
-nu <- runif(n)
+l <- rep(0, n)
+nu <- rep(0, n)
 
 for (iteration in 1:N.ITERATIONS) {
     L <- diag(l)
@@ -31,10 +32,10 @@ for (iteration in 1:N.ITERATIONS) {
     mu <- K %*% nu
 
     for (i in 1:n) { 
-	x <- rnorm(MC.SAMPLES, mean=mu[i], S[i, i])
-	v <- V(x, Y[i])
-	l[i] <- mean((x - mu[i]) * v / S[i, i])
-	nu[i] <- mean(((x - mu[i])^2 * v  - S[i, i] * v) / S[i, i])
+	x <- rnorm(MC.SAMPLES, mean=mu[i, ], S[i, i])
+	v <- V(x, Y[i], theta)
+	nu[i] <- - mean((x - mu[i, ]) * v / S[i, i])
+	l[i] <- mean(((x - mu[i, ])^2 * v  - S[i, i] * v) / S[i, i])
     }
 }
 
