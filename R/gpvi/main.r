39c73bac96f92e7b7f4da4db89dc396278d68834
mkomod <mkomodromos01@gmail.com>
2020-11-04 13:56:18 +0000

    plots for gpvi

--- R/gpvi/main.r
+++ R/gpvi/main.r
@@ -21,9 +21,7 @@ V <- function(Y, x, eta) {
 K <- kern.gauss(X, X, theta[1], theta[2])
 
 
-# Plot data
-pdf("./plots/gpvi_fit.pdf", width=12, height=7)
-par(mar=c(4,3,0,0))
+# Plot data, and data generating process
 local({
     bounds <- quantile(rlaplace(1000, eta=1.5), 0.025)
     lower <- f + bounds[1]
@@ -39,6 +37,16 @@ local({
 })
 
 # Making predictions ---------------------------------------------------------
+pdf("./plots/gpvi_fit.pdf", width=9, height=5)
+par(mar=c(4,3,0,0))
+local({
+    d.hmc <- read.csv("./results/fitsummary.csv")
+    d.hmc <- d.hmc[22:41, ]
+    plot(X, d.hmc$mean, type="l", col="black",
+	bty="n", ylim=c(-10, 5), ylab="")
+    polygon(c(X, rev(X)), c(d.hmc$X97.5., rev(d.hmc$X2.5.)), 
+    col=rgb(0, 0, 0, 0.2), border=NA)
+})
 local({
     m1 <- opt_elbo(X, Y, kern.gauss, V, theta, lnu.init=runif(2 * length(Y)))
     p <- gpvi.params(m1$par, K)
@@ -50,9 +58,18 @@ local({
 
     lines(X, gp.mean.p, col="red")
     polygon(c(X, rev(X)), c(gp.conf.p[2,], rev(gp.conf.p[1,])), 
-    col=rgb(1, 0, 0, 0.1), border=NA)
+	col=rgb(1, 0, 0, 0.07), border=NA)
+    points(X, Y, pch=17, col="blue")
+    points(X, f, pch=15, col="darkgreen")
+    legend(8, -6, legend=c("Y", "f(x)", "f(x) VI", "f(x) HMC"), 
+	   col=c("blue", "darkgreen", "red", "black"), 
+	   lty=c(NA, NA, 1, 1), pch=c(17, 15, NA, NA))
 })
 dev.off()
+
+
+
+
 
 # VSBC Diagnostic ------------------------------------------------------------
 vsbc <- parallel::mclapply(1:VSBC.SAMPLES, function(j) {
@@ -84,15 +101,15 @@ for (i in 1:num.params) {
     vsbc.pvalues[i] <- ks.test(vsbc.mat[ , i], 1-vsbc.mat[ , i])$p.value
 }
 
-# pdf("./plots/gpvi_vsbc.pdf", width=12, height=3)
-# par(mfrow=c(1,5), mar=c(3, 4, 4, 2))
-# hist(vsbc.mat[ , 1], main=expression(f[1]), xlab="")
-# par(mar=c(3, 3, 4, 2))
-# hist(vsbc.mat[ , 5],  main=expression(f[5]), xlab="")
-# hist(vsbc.mat[ , 10], main=expression(f[10]), xlab="")
-# hist(vsbc.mat[ , 15], main=expression(f[15]), xlab="")
-# hist(vsbc.mat[ , 20], main=expression(f[20]), xlab="")
-# dev.off()
+pdf("./plots/gpvi_vsbc.pdf", width=12.5, height=2.5)
+par(mfrow=c(1,5), mar=c(3, 4, 2, 2))
+hist(vsbc.mat[ , 1], main="", xlab="")
+par(mar=c(3, 3, 2, 2))
+hist(vsbc.mat[ , 5], main="", xlab="")
+hist(vsbc.mat[ , 10], main="", xlab="")
+hist(vsbc.mat[ , 15], main="", xlab="")
+hist(vsbc.mat[ , 20], main="", xlab="")
+dev.off()
 
 
 
